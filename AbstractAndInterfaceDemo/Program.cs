d3275abb267b8a6c842ff5107bfd8d5f52d40e3c
gafur-demirci <73399810+gafur-demirci@users.noreply.github.com>
2021-02-09 12:53:57 +0300

    Add project files.

---
+++ AbstractAndInterfaceDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractAndInterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
